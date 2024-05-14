15692618d27b2a0c42c26f606dcc68bb56288741
Raph <94909118+Ryustiel@users.noreply.github.com>
2024-05-14 15:35:07 +0200

    Add offer pipeline and some login checks

--- myapp/middleware/isLoggedIn.js
+++ myapp/middleware/isLoggedIn.js
@@ -1,15 +1,10 @@
-const logger = require("../logger");
-
 function isLoggedIn(req, res, next) {
-    logger.debug("Vérification de l'état de connexion de l'utilisateur...");
     if (req.session.userEmail) {
-        logger.debug(`Utilisateur connecté avec l'email : ${req.session.userEmail}`);
         next();
     } else {
-        logger.warn("Aucun utilisateur connecté, redirection vers /login");
+        req.session.returnTo = req.headers.referer || '/'; // save current page
         res.redirect('/login');
     }
 }
-
 
 module.exports = isLoggedIn;
