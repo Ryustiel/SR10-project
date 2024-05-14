052ade7da1cbea9be3e77cd3afced3850f9c3f1f
Raph <94909118+Ryustiel@users.noreply.github.com>
2024-05-14 11:03:11 +0200

    Changed form view structure

--- myapp/routes/jobs.js
+++ myapp/routes/jobs.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const model_ficheposte = require('../model/ficheposte');
+const model_offreemploi = require('../model/offreemploi');
 
 const OffreEmploi = require('../model/offreemploi'); // Ajustez le chemin selon votre structure
 
-router.get('/jobslist', async function(req, res, next) {
+router.get('/browse_offers', async function(req, res, next) {
     try {
         const offres = await OffreEmploi.readall();
         res.render('jobsList', { title: 'Liste des Offres d\'Emploi', offres: offres });
@@ -19,10 +20,39 @@ router.post('/view', function(req, res, next) {
 });
 
 router.get('/add_offer', function(req, res, next) {
-    // VIEW EXISTING POSITIONS
-    // CHECK FOR ROLE ?
-    res.render('jobs/add_offer');
+    res.render('jobs/add_offer', {
+        notification: null,
+        res_string: 'test'
+    });
 });
+
+router.post('/add_offer', async function(req, res, next) {
+    try {
+        let { dateValidite, listePieces, nombrePieces, idFiche } = req.body;
+
+        let idRecruteur = 0; // GET IT
+        let etat = "Ouverte";
+
+        await model_offreemploi.create({
+            etat,
+            dateValidite,
+            listePieces,
+            nombrePieces,
+            idFiche,
+            idRecruteur
+        });
+
+        res.render('jobs/add_position', {
+            notification: 'Votre fiche d\'emploi a été ajoutée avec succès !'
+        });
+
+    } catch (error) {
+        console.error('Add Fiche Emploi Error', error);
+        res.status(500).render('error', { message: "Erreur interne du serveur", error });
+    }
+
+});
+
 
 router.get('/add_position', function(req, res, next) {
     res.render('jobs/add_position', {
@@ -32,11 +62,11 @@ router.get('/add_position', function(req, res, next) {
 
 router.post('/add_position', async function(req, res, next) {
     try {
-        const {intitule, statutPoste, responsableHierarchique, typeMetier,
+        let {intitule, statutPoste, responsableHierarchique, typeMetier,
             lieuMission, rythme, salaire, description } = req.body;
 
         // WARNING : ADD_POSITION CURRENTLY USING DEFAULT ORGANIZATION ID 1 INSTEAD OF USER SESSION ORG ID
-        const idOrganisation = '123456789';
+        let idOrganisation = '123456789';
 
         await model_ficheposte.create({
             intitule,
@@ -48,7 +78,7 @@ router.post('/add_position', async function(req, res, next) {
             salaire,
             description,
             idOrganisation
-        })
+        });
 
         res.render('jobs/add_position', {
             notification: 'Votre fiche d\'emploi a été ajoutée avec succès !'
@@ -58,7 +88,6 @@ router.post('/add_position', async function(req, res, next) {
         console.error('Add Fiche Emploi Error', error);
         res.status(500).render('error', { message: "Erreur interne du serveur", error });
     }
-
 
 });
 
